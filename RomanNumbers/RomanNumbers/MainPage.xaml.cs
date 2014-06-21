7271a2da16269afc2c22af920d77d6b1072460bb
Alexandre Rodrigues <alexandrejbr@live.com>
2014-06-21 17:54:02 +0100

    Initial commit

---
+++ RomanNumbers/RomanNumbers/MainPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace RomanNumbers
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private const string InvalidRomanNumberErrorMessage = 
+            "Tem que introduzir um número romano válido";
+
+        private const string OverTheLimitsNumberErrorMessage = 
+            "Apenas são admitidos valores entre 1 e 3999";
+
+        private const string InvalidArabicNumberErrorMEssage =
+            "O que introduziu não é um número válido. Introduza um valor entre 1 e 3999";
+
+        private static readonly string[] ConversionTypes = 
+        { 
+            "Romana para Árabe", 
+            "Árabe para Romana"
+        };
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            this.LP_ConversionType.ItemsSource = ConversionTypes;
+        }
+
+        private void BtConvertClick(object sender, RoutedEventArgs e)
+        {
+            if(this.LP_ConversionType.SelectedIndex == 0)
+            {
+                if (this.Value_Number.Text.Length > 15)
+                {
+                    MessageBox.Show(InvalidRomanNumberErrorMessage);
+                    return;
+                }
+                int res = RomanConverter.RomanToArabic(this.Value_Number.Text);
+                if(res < 0)
+                {
+                    MessageBox.Show(InvalidRomanNumberErrorMessage);
+                    return;
+                }
+                this.Value_Result.Text = res.ToString();
+            }
+            else
+            {
+                try
+                {
+                    string arabicToRoman = 
+                        RomanConverter.ArabicToRoman(Convert.ToInt32(this.Value_Number.Text));
+                    if(arabicToRoman == null)
+                    {
+                        MessageBox.Show(OverTheLimitsNumberErrorMessage);
+                        return;
+                    }
+                    this.Value_Result.Text =
+                        arabicToRoman;
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show(OverTheLimitsNumberErrorMessage);
+                }
+                catch (FormatException)
+                {
+                    MessageBox.Show(InvalidArabicNumberErrorMEssage);
+                }   
+            }
+        }
+
+        private void LpConversionTypeSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.Value_Number.InputScope.Names.Clear();
+            this.Value_Number.InputScope.Names.Add(
+                this.LP_ConversionType.SelectedIndex == 0
+                    ? new InputScopeName {NameValue = InputScopeNameValue.PersonalNameSuffix}
+                    : new InputScopeName {NameValue = InputScopeNameValue.Number});
+
+            this.Value_Number.Text = String.Empty;
+            this.Value_Result.Text = String.Empty;
+        }
+
+        private void ValueNumberTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(this.LP_ConversionType.SelectedIndex == 0)
+            {
+                int selectionStart = this.Value_Number.SelectionStart;
+                int selectionLength = this.Value_Number.SelectionLength;
+                this.Value_Number.Text = this.Value_Number.Text.ToUpper();
+                this.Value_Number.SelectionStart = selectionStart;
+                this.Value_Number.SelectionLength = selectionLength;
+            }
+        }
+    }
+}
---
+++ RomanNumbers/RomanNumbers/RomanConverter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace RomanNumbers
+{
+    public class RomanConverter
+    {
+        private static readonly String[] Units = { "", "I", "II", "III", "V", "IV", "VI", "VII", "VIII", "IX" };
+        private static readonly String[] Tens = { "", "X", "XX", "XXX", "L", "XL", "LX", "LXX", "LXXX", "XC" };
+        private static readonly String[] Hundreds = { "", "C", "CC", "CCC", "D", "CD", "DC", "DCC", "DCCC", "CM" };
+        private static readonly String[] Thousands = { "", "M", "MM", "MMM" };
+
+        public static string ArabicToRoman(int number)
+        {
+            if (number > 0 && number < 4000)
+            {
+                int u = number % 10;
+                number = number / 10;
+                int d = number % 10;
+                number = number / 10;
+                int c = number % 10;
+                number = number / 10;
+                int m = number % 10;
+
+                StringBuilder sb = new StringBuilder();
+                sb.Append(Thousands[m]);
+                if (c == 5)
+                    sb.Append(Hundreds[c - 1]);
+                else
+                    if (c == 4)
+                        sb.Append(Hundreds[c + 1]);
+                    else
+                        sb.Append(Hundreds[c]);
+                if (d == 5)
+                    sb.Append(Tens[d - 1]);
+                else
+                    if (d == 4)
+                        sb.Append(Tens[d + 1]);
+                    else
+                        sb.Append(Tens[d]);
+                if (u == 5)
+                    sb.Append(Units[u - 1]);
+                else
+                    if (u == 4)
+                        sb.Append(Units[u + 1]);
+                    else
+                        sb.Append(Units[u]);
+                //sb.Append(Tens[d]);
+                //sb.Append(Units[u]);
+                return sb.ToString();
+                //System.out.println("Numero em numeracao romana " + numero);
+                //System.out.println();
+                //System.out.println("Introduza o numero:");
+            }
+            return null;
+        }
+
+        public static int RomanToArabic(string roman)
+        {
+            if (String.IsNullOrEmpty(roman))
+                return -1;
+            int acc = 0;
+
+            roman = roman.ToUpper();
+
+            acc += Extract(Units, roman, out roman);
+            acc += Extract(Tens, roman, out roman) * 10;
+            acc += Extract(Hundreds, roman, out roman) * 100;
+            acc += Extract(Thousands, roman, out roman) * 1000;
+
+            if (!String.IsNullOrEmpty(roman))
+                return -1;
+            return acc;
+        }
+
+        private static int Extract(string[] values, string roman, out string updatedRoman)
+        {
+            int romanLastIndex = roman.Length - 1;
+            for (int i = values.Length - 1; i > 0; i--)
+            {
+
+                bool found = true;
+
+                for (int uIndex = values[i].Length - 1, rIndex = romanLastIndex;
+                    uIndex >= 0;
+                    --uIndex, --rIndex)
+                {
+                    if (rIndex < 0)
+                    {
+                        found = false;
+                        break;
+                    }
+                    if (roman[rIndex] != values[i][uIndex])
+                        found = false;
+                }
+                if (found)
+                {
+                    if (i == 5)
+                    {
+                        updatedRoman = roman.Substring(0, roman.Length - values[i].Length);
+                        return 4;
+                    }
+                    if (i == 4)
+                    {
+                        updatedRoman = roman.Substring(0, roman.Length - values[i].Length);
+                        return 5;
+                    }
+                    updatedRoman = roman.Substring(0, roman.Length - values[i].Length);
+                    return i;
+                }
+            }
+            updatedRoman = roman;
+            return 0;
+        }
+    }
+}
